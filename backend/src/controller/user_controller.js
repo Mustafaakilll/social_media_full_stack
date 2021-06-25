f18b80017eb791ef20b16f458af74fcbe6989339
= <mustafaakil49@gmail.com>
2021-06-25 10:54:59 +0300

    CRUD operations completed for user

--- backend/src/controller/user_controller.js
+++ backend/src/controller/user_controller.js
@@ -1,57 +1,72 @@
 const userModel = require("./../model/user_model");
 // TODO: CREATE MODEL FOR RESPONSE AND ERROR
+const knex = require("knex")({
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_URL,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  },
+  migrations: {
+    tableName: process.env.DB_USER_TABLE,
+  },
+});
 
 module.exports = {
-  getAllUser: async (con, res) => {
+  getAllUser: async (res) => {
     try {
       const users = [];
-      const _user = await con("user").select();
+      const _user = await knex("user").select();
       _user.map((user) => {
         users.push(new userModel(user));
       });
       res.json(users);
     } catch (e) {
-      res.json(e);
+      console.log(e);
+      res.status(400).json(e);
     }
   },
-  getUserById: async (con, req, res) => {
+  getUserById: async (req, res) => {
     try {
       let user;
       const userId = req.params.userId;
-      const _user = await con("user").select().where("user_id", userId);
+      const _user = await knex("user").select().where("user_id", userId);
       _user.map((u) => {
         user = new userModel(u);
       });
       res.json(user);
     } catch (e) {
-      res.json(e);
+      res.status(400).json(e);
     }
   },
-  createUser: async (con, req, res) => {
+  createUser: async (req, res) => {
     //TODO: ADD USER TO AWS COGNITO SAME TIME
     try {
-      const _user = await con("user").insert(req.body);
+      const _user = await knex("user").insert(req.body);
       res.json(_user);
     } catch (e) {
-      res.json(e);
+      res.status(400).json(e);
     }
   },
-  updateUser: async (con, req, res) => {
+  updateUser: async (req, res) => {
     try {
       const userId = req.params.userId;
-      const _user = await con("user").where("user_id", userId).update(req.body);
+      const _user = await knex("user")
+        .where("user_id", userId)
+        .update(req.body);
       res.json(_user + " Satir guncellendi.");
     } catch (e) {
-      res.json(e);
+      res.status(400).json(e);
     }
   },
-  deleteUser: async (con, req, res) => {
+  deleteUser: async (req, res) => {
     try {
       const userId = req.params.userId;
-      const _user = await con("user").where("user_id", userId).delete();
+      const _user = await knex("user").where("user_id", userId).delete();
       res.json(_user + " Satir silindi.");
     } catch (e) {
-      res.json(e);
+      res.status(400).json(e);
     }
   },
 };
--- backend/src/routes/user_router.js
+++ backend/src/routes/user_router.js
@@ -1,23 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const mysql = require("mysql2");
-const knex = require("knex")({
-  client: "mysql2",
-  connection: {
-    host: process.env.DB_URL,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-  },
-  migrations: {
-    database: process.env.DB_DATABASE,
-    tableName: process.env.DB_TABLE,
-  },
-});
 
 const userController = require("./../controller/user_controller");
 
-router.get("/", (req, res) => userController.getAllUser(knex, res));
+router.get("/", (req, res) => userController.getAllUser(res));
 router.get("/:userId", (req, res) =>
   userController.getUserById(knex, req, res)
 );
